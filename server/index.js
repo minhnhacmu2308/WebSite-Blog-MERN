b7b00665b10af8477b063c5f804d06f0d8b9dbec
minhnhacmu2308 <65648352+minhnhacmu2308@users.noreply.github.com>
2021-04-26 10:20:08 +0700

    new

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import posts from './router/posts.js'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 5000;
+const URI = process.env.DATABASE_URL ;
+
+app.use(bodyParser.json({limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true , limit: "30mb" }));
+app.use(cors());
+
+app.use('/posts', posts);
+
+mongoose
+    .connect(URI, {useNewUrlParser:true , useUnifiedTopology: true})
+    .then(() => {
+        console.log("Connect Successfully!!!");
+        app.listen(PORT, () => {
+            console.log("Server running !!!!! ")
+        })
+    }).catch((err) => {
+        console.log(err)
+    })
